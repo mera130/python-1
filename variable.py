ea64c705d859579cf570e7fdeb4f794c7081166a
mera130 <mera2011.11.3@gmail.com>
2025-09-24 19:38:11 +0600

    variable

---
+++ variable.py
@@ -0,0 +1,8 @@
+x = 5
+y = "john"
+print(type(x))
+print(type(y))
+waterbottle = "wb"
+print(waterbottle)
+name = input("enter your name :")
+print("/nWelcome", name )
---
+++ welcome_coding.py
@@ -0,0 +1,5 @@
+print("welcome to the world of python")
+print(4)
+print("hello python /n")
+print("hello", 5)
+print("wlecome to", end = "*")
