2daee6f11a6e5d4f0ded35781852d41b03a5b8df
mera130 <mera2011.11.3@gmail.com>
2025-10-10 19:01:26 +0600

    weather

---
+++ weather.py
@@ -0,0 +1,11 @@
+w = int(input("what the weather there?(in celsius): "))
+
+if w > 20:
+    print('its summer')
+    print('No heavy clothes. Wear light, breathable fabrics only.')
+    
+else:
+    print('its winter')
+    print('wear heavy clothes: jacket, sweater, long pants. Essential for mornings and nights.')
+    
+print('this is only suitable for bangladesh')
