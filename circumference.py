f82fd7e7bc31cc61cad2e85f4a53ba9f9b179c2d
mera130 <mera2011.11.3@gmail.com>
2025-11-08 21:57:10 +0600

    function

---
+++ circumference.py
@@ -0,0 +1,8 @@
+def circumference():
+    radius = float(input('Enter the length of radius: '))
+    circumference = 2 * 3.1415 * radius
+    return circumference
+
+
+print("The circumference is:", circumference())
+
