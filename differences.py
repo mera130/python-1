b36d51e5e2037d64efceeb9fab4535ef74197bf7
mera130 <mera2011.11.3@gmail.com>
2026-01-07 12:05:14 +0600

    set and array

---
+++ differences.py
@@ -0,0 +1,6 @@
+set1 = {1,2,3,4,5}
+set2 = {4,5,6,7,8}
+result = set1.symmetric_difference(set2)
+print('the first set:', set1)
+print('the second set: ', set2)
+print('symmetrical differences: ', result)
