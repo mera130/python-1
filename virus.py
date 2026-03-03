45c5179ce4e1d23dee82a19f2900ece93c258e96
mera130 <mera2011.11.3@gmail.com>
2026-03-03 21:49:41 +0600

    event

---
+++ virus.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from tkinter import messagebox
+root = Tk()
+root.geometry('200x200')
+def msg():
+    messagebox.showwarning('alert!', 'a virus is found')
+button = Button(root,text='scan for virus', command = msg)
+button.place(x=40, y=80)
+root.mainloop()
+    
